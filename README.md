da4e1dcee918bd37aeaf90e7e823953c0ba3663c
Oliver Hughes <straightupdirtybass@gmail.com>
2021-02-15 17:25:46 +0000

    Added a README.md file with instructions.

---
+++ README.md
@@ -0,0 +1,6 @@
+currency-converter
+===============
+A basic currency converter Made in Python that runs in a console.
+
+INSTRUCTIONS:
+  To select the type of currency, input the number, then input the amount you want to convert.
--- main.py
+++ main.py
@@ -1,77 +1,81 @@
 import sys
 
+
 def code():
-  typeOfMoney = int(input("\nWhat type of money?\n1.Pounds \n2.Dollars \n3.Euros \n4.Japanese Yen\n"))
+    typeOfMoney = int(
+        input(
+            "\nWhat type of money?\n1.Pounds \n2.Dollars \n3.Euros \n4.Japanese Yen\n"
+        ))
 
-  #pounds
+    #pounds
 
-  if typeOfMoney == 1:
+    if typeOfMoney == 1:
 
-    Money = float(input("How much money do you want to convert?\n"))
-    dollar = "{:.2f}".format(Money*1.37)
-    euro = "{:.2f}".format(Money*1.14)
-    yen = "{:.2f}".format(Money*146.53)
-    print(f"£{Money} is ${dollar}, €{euro} and ¥{yen}")
+        Money = float(input("How much money do you want to convert?\n"))
+        dollar = "{:.2f}".format(Money * 1.37)
+        euro = "{:.2f}".format(Money * 1.14)
+        yen = "{:.2f}".format(Money * 146.53)
+        print(f"£{Money} is ${dollar}, €{euro} and ¥{yen}")
 
-    restart = input("\nDo you want to restart this program? [y/n]\n")
+        restart = input("\nDo you want to restart this program? [y/n]\n")
 
-    if restart == "y":
-      code()
-      set.clear()
-    else:
-      sys.exit()
+        if restart == "y":
+            code()
+            set.clear()
+        else:
+            sys.exit()
 
-  #dollars
+    #dollars
 
-  if typeOfMoney == 2:
+    if typeOfMoney == 2:
 
-    Money = float(input("How much money do you want to convert?\n"))
-    pounds = "{:.2f}".format(Money*0.73)
-    euro = "{:.2f}".format(Money*0.83)
-    yen = "{:.2f}".format(Money*105.38)
-    print(f"${Money} is £{pounds}, €{euro} and ¥{yen}")
+        Money = float(input("How much money do you want to convert?\n"))
+        pounds = "{:.2f}".format(Money * 0.73)
+        euro = "{:.2f}".format(Money * 0.83)
+        yen = "{:.2f}".format(Money * 105.38)
+        print(f"${Money} is £{pounds}, €{euro} and ¥{yen}")
 
-    restart = input("\nDo you want to restart this program? [y/n]\n")
+        restart = input("\nDo you want to restart this program? [y/n]\n")
 
-    if restart == "y":
-      code()
-      set.clear()
-    else:
-      sys.exit()
+        if restart == "y":
+            code()
+            set.clear()
+        else:
+            sys.exit()
 
-  #euros
+    #euros
 
-  if typeOfMoney == 3:
+    if typeOfMoney == 3:
 
-    Money = float(input("How much money do you want to convert?\n"))
-    dollars = "{:.2f}".format(Money*1.21)
-    pound = "{:.2f}".format(Money*0.88)
-    yen = "{:.2f}".format(Money*127.76)
-    print(f"€{Money} is ${dollars}, £{pound} and ¥{yen}")
+        Money = float(input("How much money do you want to convert?\n"))
+        dollars = "{:.2f}".format(Money * 1.21)
+        pound = "{:.2f}".format(Money * 0.88)
+        yen = "{:.2f}".format(Money * 127.76)
+        print(f"€{Money} is ${dollars}, £{pound} and ¥{yen}")
 
-    restart = input("\nDo you want to restart this program? [y/n]\n")
+        restart = input("\nDo you want to restart this program? [y/n]\n")
 
-    if restart == "y":
-      code()
-      set.clear()
-    else:
-      sys.exit()
+        if restart == "y":
+            code()
+            set.clear()
+        else:
+            sys.exit()
 
-  #yen
+    #yen
 
-  if typeOfMoney == 4:
+    if typeOfMoney == 4:
 
-    Money = float(input("How much money do you want to convert?\n"))
-    dollars = "{:.2f}".format(Money*0.0095)
-    pound = "{:.2f}".format(Money*0.0068)
-    euro = "{:.2f}".format(Money*0.0078)
-    print(f"¥{Money} is ${dollars}, £{pound} and €{euro}")
+        Money = float(input("How much money do you want to convert?\n"))
+        dollars = "{:.2f}".format(Money * 0.0095)
+        pound = "{:.2f}".format(Money * 0.0068)
+        euro = "{:.2f}".format(Money * 0.0078)
+        print(f"¥{Money} is ${dollars}, £{pound} and €{euro}")
 
-    restart = input("\nDo you want to restart this program? [y/n]\n")
+        restart = input("\nDo you want to restart this program? [y/n]\n")
 
-    if restart == "y":
-      code()
-      set.clear()
-    
+        if restart == "y":
+            code()
+            set.clear()
+
 
 code()
